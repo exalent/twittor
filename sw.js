3cf1f902c6f74ba349d3cb8a2316f4f8ffe5e833
exalent <paitttt@gmail.com>
2020-09-28 12:40:25 -0400

    3 commit modificacion de ruta del sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 //importar archivos extra
-importScripts('/sw/06-twittor/js/sw-utils.js');
+importScripts('js/sw-utils.js');
 
 
 const CACHE_STATIC = 'static-v1';
@@ -8,23 +8,23 @@ const CACHE_INMUTABLE = 'inmutable-v1';
 
 const APP_SHEL = [
     // '/',
-    '/sw/06-twittor/index.html',
-    '/sw/06-twittor/css/style.css',
-    '/sw/06-twittor/img/favicon.ico',
-    '/sw/06-twittor/img/avatars/hulk.jpg',
-    '/sw/06-twittor/img/avatars/thor.jpg',
-    '/sw/06-twittor/img/avatars/spiderman.jpg',
-    '/sw/06-twittor/img/avatars/ironman.jpg',
-    '/sw/06-twittor/img/avatars/wolverine.jpg',
-    '/sw/06-twittor/js/app.js',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
 ];
 
 const APP_SHEL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    '/sw/06-twittor/css/animate.css',
-    '/sw/06-twittor/js/libs/jquery.js'
+    'css/animate.css',
+    'js/libs/jquery.js'
 ];
 
 self.addEventListener('install', e =>{
